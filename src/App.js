4f5ebad4a8dc2cf2568b6d796a50885898cd800b
laps78 <9254384@mail.ru>
2023-05-12 15:06:06 +0300

    App notation added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>
+        Чтобы все заработало, запусти сервачок из папки <strong>../backend/</strong><br />
+        Он же уже установлен?(<strong>npm i</strong>)
+      </p>
       app
     </div>
   );
